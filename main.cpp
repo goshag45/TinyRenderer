ea24b5910eed854a974dbaae3dc17a2bc5e563ab
goshag45 <goshagugl44@gmail.com>
2025-11-28 20:39:02 +1100

    experimenting with time
    my benchmark sucks ass, ~14 seconds for 16m lines

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <cmath>
 #include "tgaimage.h"
 #include <cstdlib>
+#include <ctime>
 
 constexpr TGAColor white   = {255, 255, 255, 255}; // attention, BGRA order
 constexpr TGAColor green   = {  0, 255,   0, 255};
@@ -15,9 +16,6 @@ void line(int ax, int ay, int bx, int by, TGAImage &framebuffer, TGAColor color)
     float largest = (float)std::max(a_gap, b_gap);
     increment = (100 / largest) / 100;
 
-    std::printf("%f\n", largest);
-    std::printf("%f\n", increment);
-
     for (float t=0.; t<1.; t+=increment) {
         int x = std::round( ax + (bx - ax) * t);
         int y = std::round( ay + (by - ay) * t);
@@ -30,18 +28,12 @@ int main(int argc, char** argv) {
     constexpr int height = 64;
     TGAImage framebuffer(width, height, TGAImage::RGB);
 
-    int ax =  7, ay =  3;
-    int bx = 12, by = 37;
-    int cx = 62, cy = 53;
-
-    line(ax, ay, bx, by, framebuffer, blue);
-    line(cx, cy, bx, by, framebuffer, green);
-    line(cx, cy, ax, ay, framebuffer, yellow);
-    line(ax, ay, cx, cy, framebuffer, red);
-
-    framebuffer.set(ax, ay, white);
-    framebuffer.set(bx, by, white);
-    framebuffer.set(cx, cy, white);
+    std::srand(std::time({}));
+    for (int i=0; i<(1<<24); i++ ) {
+        int ax = rand()%width, ay = rand()%height;
+        int bx = rand()%width, by = rand()%height;
+        line(ax, ay, bx, by, framebuffer, { static_cast<uint8_t>(rand()%255), static_cast<uint8_t>(rand()%255), static_cast<uint8_t>(rand()%255), static_cast<uint8_t>(rand()%255) });
+    }
 
     framebuffer.write_tga_file("framebuffer.tga");
     return 0;
